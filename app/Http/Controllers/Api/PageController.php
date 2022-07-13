8e5359dd584611556ac1e79560bb871b48301351
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-07-13 15:50:50 +0200

    Aggiunta stampa su welcome page (post.category.name da rendere required in php)

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Post;
+use App\Category;
 
 class PageController extends Controller
 {
     public function index(){
-        $posts = Post::all();
-        return response()->json($posts) ;
+        $posts = Post::with('category')->with('tags')->get();
+        $categories = Category::with('posts');
+        return response()->json(compact('posts', 'categories')) ;
     }
 }
